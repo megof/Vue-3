d191fdfc34bc54e37e5682a1e5a0763460ccc886
Megof <j_do_o13@hotmail.com>
2023-01-01 14:49:23 -0500

    post - emit

---
+++ src/components/custom/BlogPost.vue
@@ -0,0 +1,23 @@
+<script setup>
+//const pro = defineProps(['title','text','txtclr'])
+console.log(pro.txtclr)
+const pro = defineProps({
+    title: String,
+    text: String,
+    txtclr:{
+        type: String,
+        default: "danger",
+    },})
+    const asd=(t)=>{
+        console.log(t)
+    }
+const usemit = defineEmits(['metchangefav'])
+</script>
+<template>
+    <div class="card" @click="usemit('metchangefav', title)">
+        <div class="card-body" >
+            <h5 class="card-title" :class="`text-${txtclr}`" >{{ title }}</h5>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+</template>
---
+++ src/components/custom/ButtonCounter.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue';
+
+const counter = ref(0);
+const incremet = () => counter.value++;
+</script>
+<template>
+    <button @click="incremet">Counter {{ counter }}</button>
+</template>
